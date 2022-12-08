1cb988fce7a115d9d60a9215aff00f1ffd361886
boyscout99 <tommaso.praturlon@gmail.com>
2022-12-08 11:43:27 +0100

    Changed temeperature parameter to use cmd line defined delta

--- src/main/java/se/kth/jabeja/Jabeja.java
+++ src/main/java/se/kth/jabeja/Jabeja.java
@@ -56,7 +56,7 @@ public class Jabeja {
     // Typical choices for alpha are between 0.8 and 0.99
     //  T -= config.getDelta(); // linear decrease, old
     System.out.printf("T: %f\n", T);
-    T = T*0.9f;
+    T = T*config.getDelta();
     if (T < 0.00001f)
       T = 0.00001f;
   }
